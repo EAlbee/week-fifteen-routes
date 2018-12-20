8f6b46de93688743e793f100398392febec91f0e
EAlbee <albee_eric@hotmail.com>
2018-12-19 20:43:42 -0600

    added parameter

--- src/app/cat/cat.component.ts
+++ src/app/cat/cat.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Routes, ROUTES } from '@angular/router';
+
+interface catName {
+  name: string;
+}
 
 @Component({
   selector: 'app-cat',
   templateUrl: './cat.component.html',
   styleUrls: ['./cat.component.css']
 })
-
 export class CatComponent implements OnInit {
-  
-  catName = this.route.params.subscribe(params.name);
 
-  constructor(private route: ActivatedRoute) { }
+  catName ="";
+  //temp = "ERIC";
 
-  ngOnInit() {
+  constructor(private routes: ActivatedRoute) { 
+
   }
 
- 
+  getCatName () {
+    //this.catName = this.routes.params['name'];
+    //this.catName = this.routes.queryParams.subscribe(params => this.catName = params[];)
+    //this.catName=this.routes.params.subscribe( params => params.name);
+    this.routes.params.subscribe( params => console.log(params['name']) );
+    this.routes.params.subscribe( params => this.catName=(params['name']) )
+    console.log("catName:" + this.catName);
+    //this.temp = "Cathy";
+  }
 
-  public getCatName() {
-    this.catName = this.route.params.subscribe()
-
+  ngOnInit() {
+    this.getCatName();
+    
   }
 
 }
+
